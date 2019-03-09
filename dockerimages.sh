c9955173cac59b874bd58f9003e86765cf59d50d
kiyor <ops@kiyor.com>
2019-03-08 16:56:29 -0800

    custom

--- dockerimages.sh
+++ dockerimages.sh
@@ -1,6 +1,24 @@
 #!/bin/bash -e
 
-docker build --tag="kubernautslabs/jmeter-base:latest" -f Dockerfile-base .
-docker build --tag="kubernautslabs/jmeter-master:latest" -f Dockerfile-master .
-docker build --tag="kubernautslabs/jmeter-slave:latest" -f Dockerfile-slave .
-docker build --tag="kubernautslabs/jmeter-reporter" -f Dockerfile-reporter .
+#docker build --tag="kubernautslabs/jmeter-base:latest" -f Dockerfile-base .
+#docker build --tag="kubernautslabs/jmeter-master:latest" -f Dockerfile-master .
+#docker build --tag="kubernautslabs/jmeter-slave:latest" -f Dockerfile-slave .
+#docker build --tag="kubernautslabs/jmeter-reporter" -f Dockerfile-reporter .
+
+namespace=quay.io/kiyor
+oldnamespace=kubernautslabs
+
+if [ ! -z $oldnamespace ]; then
+	for f in Dockerfile-master Dockerfile-slave jmeter_master_deploy.yaml jmeter_slaves_deploy.yaml jmeter_grafana_reporter.yaml; do
+		gsed -i "s|$oldnamespace|$namespace|g" $f
+	done
+fi
+
+for sub in base master slave reporter; do
+	img="$namespace/jmeter-$sub:latest"
+	docker build -t $img -f Dockerfile-$sub .
+done
+for sub in base master slave reporter; do
+	img="$namespace/jmeter-$sub:latest"
+	echo "docker push $img"
+done
